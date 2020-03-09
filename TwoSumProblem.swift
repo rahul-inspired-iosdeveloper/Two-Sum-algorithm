d46d47565e63d56a6a27d9fa40814f0048b3402d
Rahul K Rajan <rahul.eferns@gmail.com>
2020-03-09 15:39:28 +0530

    Create TwoSumProblem.swift

---
+++ TwoSumProblem.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+func twoSum(array:[Int],target:Int)->(Int,Int)?{
+    
+    var dict = [Int:Int]()
+    
+    //For every number
+    for (arrayItemIndex,n) in array.enumerated() {
+        
+        //Find the complement number to n, that would sum up to the target.
+        let complement = target - n
+        
+        //Checking for complement number in the dictionary
+        if let complementIndex = dict[complement] {
+            return (complementIndex,arrayItemIndex)
+        }
+        
+        //Store n with index
+        dict[n] = arrayItemIndex
+    }
+    
+    return nil
+}
+
+let result = twoSum(array: [1,5,6,7,8], target: 11)
+print(result)
